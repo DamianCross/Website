906c030bcc131bc74ac2aa2ec352a83cdc2da282
Damian Cross <damiancross@Damians-MacBook-Pro.local>
2020-07-24 00:26:14 -0400

    first commit

---
+++ scripts.mjs
@@ -0,0 +1,14 @@
+
+    function openNav()
+    {
+        document.getElementById("mySidenav").style.width = "500px";
+        document.getElementById("main").style.marginLeft = "500px";
+        document.body.style.backgroundColor = "rgba(211,211,211,0,4)";
+    }
+
+    function closeNav()
+    {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        // document.body.style.backgroundColor = "grey";
+    }
